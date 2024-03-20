4ca8bd242e026491cd02e91d029f6cc96fec538f
Vicky <159647617+vbuzzbee@users.noreply.github.com>
2024-03-20 18:52:57 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+my_name = input("What is your name? ")
+print("Hello " + my_name)
+print("awesome! I have a question for you" + my_name)
+capitols_names = input(
+    "Can you name the cities and capitols of some USA States? ")
+capitols_names = {
+    "Maryland": "Annapolis",
+    "New York": "Albany",
+    "Texas": "Austin",
+}
+print("Great! Now I will test your knowledge.")
+user_capitols = input("What is the capitol of Maryland? ")
+print(user_capitols)
